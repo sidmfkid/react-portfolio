b58eed3686d4aecf6a0f3b3045481441e567f0e3
sidmfkid <newskinlifestore@gmail.com>
2022-03-23 14:14:42 -0400

    Backdrop added to modal

--- src/components/UI/Modal.js
+++ src/components/UI/Modal.js
@@ -1,28 +1,37 @@
 import Card from "./Card";
 import ReactDOM from "react-dom";
 import React from "react";
+
+const Backdrop = (props) => {
+    return <div className="back-drop"></div>
+}
+
 const ModalOverlay = (props) => {
   return (
-    <Card>
-      <div className="modal-heading">
-        <span>Welcome to Sidtube</span>
-      </div>
-      <div className="modal-subheading">
-        <span>Let me show you around the place.</span>
-      </div>
-      <div className="modal-button">
-        <button>
-          <span>Next</span>
-          <img src={props.ArrowIcon} alt="Next Button"></img>
-        </button>
-      </div>
-    </Card>
+      <><Backdrop /><Card>
+          <div className="modal-heading">
+              <span>Welcome to Sidtube</span>
+          </div>
+          <div className="modal-subheading">
+              <span>Let me show you around the place.</span>
+          </div>
+          <div className="modal-button">
+              <button>
+                  <span>Next</span>
+                  <img src={props.ArrowIcon} alt="Next Button"></img>
+              </button>
+          </div>
+      </Card></>
   );
 };
 
 const Modal = (props) => {
   return (
     <React.Fragment>
+              {ReactDOM.createPortal(
+        <Backdrop />,
+        document.getElementById("backdrop-root")
+      )}
       {ReactDOM.createPortal(
         <ModalOverlay ArrowIcon={props.ArrowIcon} />,
         document.getElementById("overlay-root")
