36e73fe071aed26d55c040d9594ab8c49d72174e
sidmfkid <newskinlifestore@gmail.com>
2022-05-18 13:59:25 -0400

    removed cors

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 
 const express = require("express");
 const app = express();
-const cors = require("cors");
+// const cors = require("cors");
 const path = require("path");
 const morgan = require("morgan");
 const mongoose = require("mongoose");
